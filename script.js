ca5e52c4a955612dedf0c5ad6861aef6edca7fab
shreyansh <shreyansh3536@gmail.com>
2024-06-24 10:27:55 +0545

    MobileR1

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ setInterval(function(){
         counter=1;
     }
 }, 5000);
+
+
+
+
+
+function toggleNavbar() {
+    var navbarLinks = document.getElementById("nav-links-ph");
+    if (navbarLinks.style.display === "none") {
+        navbarLinks.style.display = "flex";
+    } else {
+        navbarLinks.style.display = "none";
+    }
+}
+
+  
