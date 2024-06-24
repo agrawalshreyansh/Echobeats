6199be47978a100603ad5588250cd424c2506b26
Raunak Singh <anuragkumartiwari16@gmail.com>
2024-06-24 12:23:26 +0530

    added changes

--- script.js
+++ script.js
@@ -8,15 +8,24 @@ setInterval(function(){
 }, 5000);
 
 
-
-
-
-function toggleNavbar() {
-    var navbarLinks = document.getElementById("nav-links-ph");
-    if (navbarLinks.style.display === "none") {
-        navbarLinks.style.display = "flex";
-    } else {
+function toggleNavbar(parameter) {
+    var navbarLinks = document.getElementById("navbar-links");
+    if (parameter === true) {
+        navbarLinks.style.display = "block";
+        navbarLinks.children[0].style.display = "block";
+        document.querySelector('.navbar').style.display='block';
+        document.getElementById('closenavbar').style.display='block';
+        document.getElementById('closenavbar').classList.add('placeclosebtn');
+        document.querySelector('.toggle-button').style.display='none';
+        parameter=false;
+    }
+    else{
         navbarLinks.style.display = "none";
+        navbarLinks.children[0].style.display = "flex";
+        document.querySelector('.navbar').style.display='flex';
+        document.getElementById('closenavbar').style.display='none';
+        document.querySelector('.toggle-button').style.display='block';
+        parameter=true;
     }
 }
 
